5ec915878d8fc2c7252ed9ec5fcc07a1503fe603
koda-7 <freelife17.417@gmail.com>
2020-12-25 19:58:38 +0900

    ユーザー：モデルの設定
    バリデーション、アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  wits_options presence: true do
+    validates :nickname
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :profile
+    validates :password, length: { minimum: 6 },
+                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  end
+
+  has_many :breeds
 end
