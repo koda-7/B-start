b0e2ae5c35f859e36bbac769ca668edf63bd7110
koda-7 <freelife17.417@gmail.com>
2020-12-26 17:06:43 +0900

    コントローラーの変更
    new,createアクションの設定

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,4 +1,23 @@
 class BreedsController < ApplicationController
   def index
   end
+
+  def new
+    @breed = Breed.new
+  end
+
+  def create
+    @breed = Breed.new(breed_params)
+    if @breed.save
+      redirect_to root_path
+    else
+      render.new
+    end
+  end
+
+  private
+
+  def breed_params
+    prams.require(:breed).permit(:title, :category_id, :type_name, :data, :result, :text, :image).maerge(uesr_id: current_user.id)
+  end
 end
