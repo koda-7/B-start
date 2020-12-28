aaeedb9922f551b7831286b5e6323466e8fd79a7
koda-7 <freelife17.417@gmail.com>
2020-12-28 23:36:11 +0900

    投稿機能の画像処理設定

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -7,6 +7,7 @@ class BreedsController < ApplicationController
   end
 
   def create
+    # binding.pry
     @breed = Breed.new(breed_params)
     if @breed.save
       redirect_to root_path
@@ -16,8 +17,8 @@ class BreedsController < ApplicationController
   end
 
   private
-
   def breed_params
     params.require(:breed).permit(:title, :category_id, :type_name, :date, :result, :note, :image).merge(user_id: current_user.id)
   end
+
 end
--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,18 +1,20 @@
 class Breed < ApplicationRecord
-  belongs_to :uesrs
-  has_one_attached :informations
-  # has_one_attached :image
+  belongs_to :uesr
+  has_one_attached :image
+  # has_one :informations
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 
   with_options presence: true do
     validates :title
     validates :category_id
     validates :type_name
     validates :date
+    validates :result
+    validates :note
+    validates :image
   end
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-
   validates :category_id, numericality: { other_than: 1 }
-
 end
