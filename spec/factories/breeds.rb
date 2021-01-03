7dfac5d112c3e5179e016d145cadfe953cb2d2ac
koda-7 <freelife17.417@gmail.com>
2021-01-04 00:14:00 +0900

    テスト検証
    Breedsの投稿時のテスト検証

--- spec/factories/breeds.rb
+++ spec/factories/breeds.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :breed do
+    title            { 'タイトル' }
+    category_id      { 2 }
+    type_name        { '種類名'}
+    date             { 2020-01-01 }
+    result           { '結果' }
+    note             { 'メモ' }
+
+    association :user
     
+    after(:build) do |breed|
+      breed.image.attach(io: File.open('public/images/test_iamge.png'), filename: 'test_iamge.png')
+    end 
   end
 end
--- spec/models/breed_spec.rb
+++ spec/models/breed_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
+describe Breed, type: :model do
+  before do
+    @breed = FactoryBot.build(:breed)
+  end
 
-RSpec.describe Breed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '繁殖の投稿内容' do
+    it '全ての項目の入力が存在すれば投稿できること' do
+      expect(@breed).to be_valid
+    end
+
+    it 'titleが空だと登録できないこと' do
+      @breed.title = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'category_idが空だと登録できないこと' do
+      @breed.category_id = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'type_nameが空だと登録できないこと' do
+      @breed.type_name = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Type name can't be blank")
+    end
+
+    it 'dateが空だと登録できないこと' do
+      @breed.date = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Date can't be blank")
+    end
+
+    it 'resultが空だと登録できないこと' do
+      @breed.result = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Result can't be blank")
+    end
+
+    it 'noteが空だと登録できないこと' do
+      @breed.note = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Note can't be blank")
+    end
+
+    it 'imageが空だと登録できないこと' do
+      @breed.image = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @breed.user = nil
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include('User must exist')
+    end
+
+    it 'category_idが１だと登録できないこと' do
+      @breed.category_id = 1
+      @breed.valid?
+      expect(@breed.errors.full_messages).to include("Category must be other than 1")
+    end
+  end
 end
