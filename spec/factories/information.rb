10c85210d510f696c2d658eedcdb3e499d27efb1
koda-7 <freelife17.417@gmail.com>
2021-01-09 12:14:48 +0900

    入力から保存／テスト確認まで
    幼虫飼育情報の入力から保存まで完了
    テスト実行済み

--- spec/factories/information.rb
+++ spec/factories/information.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :information do
-    
+    breedname        { 'タイトル' }
+    locality         { '産地です' }
+    generation_id    { 2 }
+    food_id          { 2 }
+    memo             { 'メモ' }
+
+    association :breed
   end
 end
--- spec/models/information_spec.rb
+++ spec/models/information_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
+describe Breed, type: :model do
+  before do
+    @information = FactoryBot.build(:information)
+  end
+  
+  describe '幼虫飼育情報' do
+    it '全ての項目の入力が存在すれば投稿できること' do
+      expect(@information).to be_valid
+    end
 
-RSpec.describe Information, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'breednameが空だと登録できないこと' do
+      @information.breedname = nil
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Breedname can't be blank")
+    end
+
+    it 'localityが空だと登録できないこと' do
+      @information.locality = nil
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Locality can't be blank")
+    end
+
+    it 'generation_idが空だと登録できないこと' do
+      @information.generation_id = nil
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Generation can't be blank")
+    end
+
+    it 'food_idが空だと登録できないこと' do
+      @information.food_id = nil
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Food can't be blank")
+    end
+
+    it 'generation_idが１だと登録できないこと' do
+      @information.generation_id = 1
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Generation must be other than 1")
+    end
+
+    it 'food_idが１だと登録できないこと' do
+      @information.food_id = 1
+      @information.valid?
+      expect(@information.errors.full_messages).to include("Food must be other than 1")
+    end
+
+    it 'memoが空白でも登録できること' do
+      @information.memo = nil
+      expect(@information).to be_valid
+    end
+
+    it 'breedが紐付いていないと保存できないこと' do
+      @information.breed = nil
+      @information.valid?
+      expect(@information.errors.full_messages).to include('Breed must exist')
+    end
+  
+
+  end
 end
