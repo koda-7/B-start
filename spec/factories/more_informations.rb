525a9743c7c9521be83b9c935007ff5a56481c17
koda-7 <freelife17.417@gmail.com>
2021-01-11 00:41:26 +0900

    テスト確認
    Moreinformationの入力テストの確認

--- spec/factories/more_informations.rb
+++ spec/factories/more_informations.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :more_information do
-    
+    number    { 1 }
+    size      { 90 }
+    sex_id    { 1 }
+    birthday  { 2020-01-01 }
+    memos     { 'メモ' }
+
+    association :information
   end
 end
--- spec/models/more_information_spec.rb
+++ spec/models/more_information_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+describe MoreInformation, type: :model do
+  before do
+    @more_information = FactoryBot.build(:more_information)
+  end
 
-RSpec.describe MoreInformation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '個別飼育情報' do
+    it '全ての項目の入力が存在すれば投稿できること' do
+      expect(@more_information).to be_valid
+    end
+
+    it 'numberが空だと登録できないこと' do
+      @more_information.number = nil
+      @more_information.valid?
+      expect(@more_information.errors.full_messages).to include("Number can't be blank")
+    end
+
+    it 'sizeが空白でも登録できること' do
+      @more_information.size = nil
+      expect(@more_information).to be_valid
+    end
+
+    it 'sex_idが1でも登録できること' do
+      @more_information.size = 1
+      expect(@more_information).to be_valid
+    end
+
+    it 'birthdayが空白でも登録できること' do
+      @more_information.birthday = nil
+      expect(@more_information).to be_valid
+    end
+
+    it 'メモが空白でも登録できること' do
+      @more_information.memos = nil
+      expect(@more_information).to be_valid
+    end
+
+    it 'informationが紐付いていないと保存できないこと' do
+      @more_information.information = nil
+      @more_information.valid?
+      expect(@more_information.errors.full_messages).to include('Information must exist')
+    end
+  end
 end
